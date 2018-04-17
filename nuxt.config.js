732bae54f3084a52c8f4f3312535bbc850727ce9
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2018-04-17 02:53:25 +0200

    Added typography styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,11 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' },
     ],
-    link: [],
+    link: [
+      // @ToDo: font & weight selection
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Lato:300,400,700,900' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Noto+Serif:400,400i,700,700i' },
+    ],
   },
   /*
   ** Global CSS
