2d1771dae796bc01888b461a418ce325b7621737
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2018-05-28 01:07:54 +0200

    Removed prosemirror

--- pages/index.vue
+++ pages/index.vue
@@ -9,44 +9,8 @@
 </template>
 
 <script>
-import { EditorView } from 'prosemirror-view';
-import { EditorState } from 'prosemirror-state';
-import { Schema, DOMParser } from 'prosemirror-model';
-import { addListNodes } from 'prosemirror-schema-list';
-import { keymap } from 'prosemirror-keymap';
-import { baseKeymap } from 'prosemirror-commands';
-import { history } from 'prosemirror-history';
-
-import { schema } from '~/components/editor/schema';
-import buildKeymap from '~/components/editor/keymap';
-import { buildInputRules } from '~/components/editor/inputrules';
-
-const mySchema = new Schema({
-  nodes: addListNodes(schema.spec.nodes, 'paragraph block*', 'block'),
-  marks: schema.spec.marks,
-});
-
-function initEditor() {
-  const content = document.getElementById('content');
-  const editor = document.getElementById('editor');
-  const state = EditorState.create({
-    mySchema,
-    doc: DOMParser.fromSchema(mySchema).parse(content),
-    plugins: [
-      buildInputRules(mySchema),
-      keymap(buildKeymap(mySchema)),
-      keymap(baseKeymap),
-      history(),
-    ],
-  })
-  const view = new EditorView(editor, { state });
-  content.style.display = 'none';
-}
-
 export default {
-  mounted() {
-    initEditor();
-  },
+  mounted() {},
 };
 </script>
 
