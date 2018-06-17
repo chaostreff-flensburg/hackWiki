04012fa0622e11d5e57398bd5493f71e750b37fc
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2018-06-17 04:11:28 +0200

    Added icon component

---
+++ components/icon.vue
@@ -0,0 +1,38 @@
+<template>
+	<svg v-html="icon"></svg>
+</template>
+
+<script>
+// List of available icons: https://feathericons.com/
+import feather from 'feather-icons';
+
+export default {
+	props: {
+		name: {
+			type: String,
+			required: true,
+			default: 'x',
+		}
+	},
+	computed: {
+		icon() {
+			return feather.icons[this.name].contents;
+		},
+	},
+}
+</script>
+
+
+<style lang="scss" scoped>
+svg {
+	width: 24px;
+  height: 24px;
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+
+	cursor: pointer;
+}
+</style>
