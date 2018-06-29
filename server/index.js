c91d865d459d7562c9af0ce2dd80460560d03755
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2018-06-29 03:58:38 +0200

    Removed nuxt

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import { Nuxt, Builder } from 'nuxt';
 
 import api from './app';
 
@@ -11,23 +10,6 @@ app.set('port', port);
 
 // Import API Routes
 app.use('/api', api);
-
-// Import and Set Nuxt.js options
-const config = require('../nuxt.config.js');
-
-config.dev = !(process.env.NODE_ENV === 'production');
-
-// Init Nuxt.js
-const nuxt = new Nuxt(config);
-
-// Build only in dev mode
-if (config.dev) {
-  const builder = new Builder(nuxt);
-  builder.build();
-}
-
-// Give nuxt middleware to express
-app.use(nuxt.render);
 
 // Listen the server
 const server = app.listen(port, host);
